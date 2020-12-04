f103cee37f20a7bb4cbf2f7015db1997fad17b2e
Nurkut <50016796+NrktSLL@users.noreply.github.com>
2020-12-04 14:59:00 +0300

    Add files via upload

---
+++ Centos8-Install-Docker-Compose.sh
@@ -0,0 +1,8 @@
+sudo dnf install curl -y
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
+echo "Latest Docker Compose Version:" $COMPOSE_VERSION
+echo | tail -n1
+sudo curl -L "https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo | tail -n1
+docker-compose --version
